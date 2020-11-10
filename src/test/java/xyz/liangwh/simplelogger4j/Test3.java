fc607ae7b6b33d02db4acd271f5fb78150b0247b
liangwh <0027015537@iwhalecloud.com>
2020-11-10 22:23:31 +0800

    增加测试样例

--- src/test/java/xyz/liangwh/simplelogger4j/Test3.java
+++ src/test/java/xyz/liangwh/simplelogger4j/Test3.java
@@ -7,6 +7,10 @@ import xyz.liangwh.simplelogger4j.core.loggers.FileLogger;
 import xyz.liangwh.simplelogger4j.core.loggers.Logger;
 import xyz.liangwh.simplelogger4j.core.manage.LoggerManager;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
@@ -163,8 +167,42 @@ public class Test3 {
             }
 
         }
+    }
 
+    @Test
+    public  void format4() throws IOException {
+        File file = new File("D:\\fw.log");
+        FileWriter fileWriter =new FileWriter(file);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
 
+        int iLoopTimes = 100;
+        int test_times = 10000;
+        long start = System.currentTimeMillis();
+        //final CountDownLatch latch = new CountDownLatch(tn);
+        //AppendRegistrant.getInstance().stop();
+        for(int j=0;j<iLoopTimes;j++){
+            for(int i=0;i<test_times;i++){
+                printWriter.printf("INTERGET:%d|STRING:%s|FLOAT:%f\n",i,"awsadsadwsad",3.2);
 
+                //logger.fPrint("%d\n",i);
+                //String format = String.format("%s   %f" ,"2a1",1.2);
+                //String forma = format("INTERGET:%d|STRING:%s|FLOAT:%f\n",1,"ssss",2.3);
+            }
+            for(int i=test_times-1;i>j;i--){
+                printWriter.printf("INTERGET:%d|STRING:%s|FLOAT:%f\n",i,"awsadsadwsad",3.2);
+                //logger.fPrint("%d\n",i);
+                //String format = String.format("%s   %f" ,"2a1",1.2);
+                //String forma = format("INTERGET:%d|STRING:%s|FLOAT:%f\n",1,"ssss",2.3);
+            }
+        }
+        long end = System.currentTimeMillis();
+        try {
+            //latch.await();
+            System.out.println("use time:"+(end-start)+"ms");
+            TimeUnit.MILLISECONDS.sleep(2000);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
